c00b69485dcb299c00fe61d4a963ec2ed80c49ac
gusBalasuriya <A.Balasuriya@btinternet.com>
2018-04-21 14:59:00 +0100

    add selectors

--- scripts/example1.js
+++ scripts/example1.js
@@ -1,3 +1,20 @@
 $(function() {
+
+    // Selecting elements by their attributes 
     $('#myContent').html('Hello World');
+    $('td[style="width:30%;"],td[style="width:70%;"]').each(function(){
+        $(this).css('border', '3px solid red');
+    });
+
+    // Selecting specific inputs in form
+    $('input[type="text"').each(function(){
+        $(this).css('background-color', 'Yellow');
+    });
+
+    // Selecting all inputs in form
+    $(':input').each(function(){
+        console.log(this);
+        $(this).val('Set by input selector');
+    });
+
  });
