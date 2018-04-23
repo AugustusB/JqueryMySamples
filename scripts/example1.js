558a7081c5239cdb6e81101caa087d5a2658dd41
gusBalasuriya <A.Balasuriya@btinternet.com>
2018-04-23 17:02:53 +0100

    implement module pattern

--- scripts/example1.js
+++ scripts/example1.js
@@ -1,20 +1,46 @@
-$(function() {
+(function(ns1, ns2) {
 
-    // Selecting elements by their attributes 
-    $('#myContent').html('Hello World');
-    $('td[style="width:30%;"],td[style="width:70%;"]').each(function(){
-        $(this).css('border', '3px solid red');
-    });
+    ns2.Ex1 = function Ex1(){
+        //private members 
+        var selectById = function selectById(id){
+            $('#' + id).html('Hello World');
+        },
+        // Selecting elements by their attributes 
+        selectByAttribute = function selectByAttribute(attribute){
+            $(attribute).each(function(){
+                $(this).css('border', '3px solid red');
+            });
+        },
+        
+        selectInput = function selectInput(input){
+         // Selecting specific inputs in form
+            $(input).each(function(){
+                $(this).css('background-color', 'Yellow');
+            });
+        },
+        
+        selectAllInputs = function selectAllInputs(allInput){
+            // Selecting all inputs in form
+            $(allInput).each(function(){
+                console.log(this);
+                $(this).val('Set by input selector');
+            });
+        },
+    
+        selectByContains = function selectByContains(contians){
+            $(contians).each(function (){
+                $(this).html("changed by contains selector");
+            });
+        };
+    
+        //public members 
+        return {
+            selectById : selectById,
+            selectByAttribute: selectByAttribute,
+            selectInput: selectInput,
+            selectAllInputs: selectAllInputs,
+            selectByContains: selectByContains
+        };
+    };
 
-    // Selecting specific inputs in form
-    $('input[type="text"').each(function(){
-        $(this).css('background-color', 'Yellow');
-    });
-
-    // Selecting all inputs in form
-    $(':input').each(function(){
-        console.log(this);
-        $(this).val('Set by input selector');
-    });
-
- });
+ })(window.Gus = window.Gus || {}, window.Gus.ExJq = window.Gus.ExJq || {});
